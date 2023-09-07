0eed899eaaada9aba3ee6aecf943a4a4c0a0daa5
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-09-07 21:07:05 +0000

    Playing with starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,37 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
+const movieID = process.argv[2];
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+// takes url and returns promise that resolve to resp obj
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    request.get(url, {}, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+
+      if (response.statusCode !== 200) {
+        return reject(new Error(response.statusMessage));
+      }
+
+      resolve(JSON.parse(response.body));
+    });
   });
-}
+};
+
+const main = async () => {
+  if (movieID) {
+    const film = await requestPromise(
+      `https://swapi-api.alx-tools.com/api/films/${movieID}`
+    );
+
+    if (film) {
+      for (const url of film.characters) {
+        const person = await requestPromise(url);
+        console.log(person.name);
+      }
+    }
+  }
+};
+main().then(() => {});
