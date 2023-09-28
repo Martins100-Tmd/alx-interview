49b5dd69af8703f5034bbaed100e27bf4d1d0adb
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-09-28 16:26:18 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,10 +5,28 @@ Task 0's module
 
 
 def island_perimeter(grid):
-    '''
-    find island perimter of grid ocean
-    '''
-
-    gridLand = [land for area in grid for land in area if land > 0]
-    boxRep = list(map(lambda x: x * 2, gridLand))
-    return sum(boxRep, 0) + 2
+    # base case
+    if not grid:
+        return 0
+ 
+    M, N = len(grid), len(grid[0])
+    count = 0
+ 
+    # traverse each cell of the matrix
+    for i in range(0, M):
+        for j in range(0, N):
+            # if the current cell is a land
+            if grid[i][j] == 1:
+                # check if top edge is adjacent to the water
+                if i == 0 or grid[i - 1][j] == 0:
+                    count = count + 1
+                # check if bottom edge is adjacent to the water
+                if i == M - 1 or grid[i + 1][j] == 0:
+                    count = count + 1
+                # check if left edge is adjacent to the water
+                if j == 0 or grid[i][j - 1] == 0:
+                    count = count + 1
+                # check if right edge is adjacent to the water
+                if j == N - 1 or grid[i][j + 1] == 0:
+                    count = count + 1
+    return count
