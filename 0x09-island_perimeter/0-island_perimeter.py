1ec8ab6c3a9bb697ff2ec2c1750fdfe612cfd990
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-09-28 16:15:18 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,14 +12,3 @@ def island_perimeter(grid):
     gridLand = [land for area in grid for land in area if land > 0]
     boxRep = list(map(lambda x: x * 2, gridLand))
     return sum(boxRep, 0) + 2
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0]
-]
-print(island_perimeter(grid))
